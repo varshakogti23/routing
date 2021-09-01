8ea2b2c1c91e4e4f34e539a1adf7d2065f13bc3b
varshakogti23 <varshakogti@gmail.com>
2021-09-01 08:29:20 +0530

    1 file modified

--- src/Nav.js
+++ src/Nav.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
     <nav>
       <h3>LOGO</h3>
       <ul classname="nav-links">
-        <li>About</li>
-        <li>Shop</li>
+        <Link to="/About">
+          <li>About</li>
+        </Link>
+        <Link to="/Shop">
+          <li>Shop</li>
+        </Link>
       </ul>
     </nav>
   );
